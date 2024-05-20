3e7e1800cfbf5e6863486835ca7a99a1d0fd03f9
Zwebelee <andi.eugster.5@gmail.com>
2024-05-20 20:38:46 +0200

    🍱 ThemeToggle Button & Assets

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+// theme toggle
+const themeToggle = document.querySelector('.theme-toggle');
+const themeIcon = document.querySelector('.theme-icon')
+
+themeToggle.addEventListener('click', () => {
+  if (themeIcon.src.includes('light_mode.svg')) {
+    themeIcon.src = 'assets/dark_mode.svg';
+  } else {
+    themeIcon.src = 'assets/light_mode.svg';
+  }
+})
