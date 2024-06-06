7e8e25513af37d0cd52660e105b3ff24941acf1f
Zwebelee <37217352+Zwebelee@users.noreply.github.com>
2024-06-06 19:08:10 +0200

    🚧 wip form handlebars

--- source/public/scripts/controllers/form-controller.js
+++ source/public/scripts/controllers/form-controller.js
@@ -24,13 +24,22 @@ export default class FormController {
   }
 
   populateForm() {
+    debugger;
     const urlParams = new URLSearchParams(window.location.search);
     const guid = urlParams.get("guid");
+
+    const templateSource = document.getElementById("button-template").innerHTML;
+    const template = Handlebars.compile(templateSource);
+
     // TODO: make a "getNode" on todostore -> get them from store, not from localstorage
     // if guid is wrong -> warning / alert invalid guid
     if (guid) {
       const todos = JSON.parse(localStorage.getItem("simple-todos"));
       const targetTodo = todos.find((todo) => todo.guid === guid);
+      const data = { isUpdate: !targetTodo };
+      const buttonsHtml = template(data);
+      document.querySelector(".form-row-submitting").innerHTML = buttonsHtml;
+
       if (targetTodo) {
         this.titleInput.value = targetTodo.title;
         this.importanceInput.value = targetTodo.importance;
@@ -53,36 +62,35 @@ export default class FormController {
   handleSubmit(event) {
     event.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
-    const guid = urlParams.get('guid');
+    const guid = urlParams.get("guid");
     if (guid) {
-      if(todoStore.checkGuid(guid)) {
-        console.log('found guid - update')
+      if (todoStore.checkGuid(guid)) {
+        console.log("found guid - update");
         const updateParams = {
           title: this.titleInput.value,
           description: this.descriptionInput.value,
           dueDate: this.dueDateInput.value,
           importance: this.importanceInput.value,
           finished: this.finishedInput.checked,
-        }
+        };
 
         todoStore.updateTodo(guid, updateParams);
       } else {
-        window.alert('No todo found for this guid.')
+        window.alert("No todo found for this guid.");
       }
-
     } else {
       todoStore.addTodo(
         this.titleInput.value,
         this.descriptionInput.value,
         this.dueDateInput.value,
         this.importanceInput.value,
-        this.finishedInput.checked,
+        this.finishedInput.checked
       );
       this.resetForm();
     }
   }
 
-  resetForm(){
+  resetForm() {
     this.form.reset();
   }
 }
