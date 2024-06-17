502484c6952f3b4e765703e7ba7bc3aac4337be2
Zwebelee <andi.eugster.5@gmail.com>
2024-06-18 01:06:44 +0200

    ✨ Backend

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path, {dirname} from 'path';
+import { fileURLToPath } from "url";
 import { todoRoutes } from './src/routes/todo-routes.js';
 
-
+const localDirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 // middlewares
-app.use(express.static('src/public'));
+app.use(express.static(path.resolve('src/public/html')));
+app.use(express.static(path.resolve('src/public')));
+
+
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile("/html/index.html", {root: `${localDirname}/src/public/`});
+});
+app.get("/form", (req, res) => {
+  res.sendFile("/html/form.html", {root: `${localDirname}/src/public/`});
+});
+
+
 app.use("/todos", todoRoutes);
 
 app.use((err, req, res, next) => {
